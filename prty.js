c38e5032173e251eaa861f6c243ae32bbf006e33
JoeBruni <jnbruni03@gmail.com>
2018-11-10 01:22:07 -0500

    Commit#1

---
+++ prty.js
@@ -0,0 +1,8 @@
+
+function showDiv() {
+   document.getElementById('menuDiv').style.display = "block";
+   document.getElementById('hideDiv').style.display="none";
+   document.getElementById('welcome').style.display="none";
+}
+/*https://codepen.io/anon/pen/wQMxBE*/
+/*https://codepen.io/rauldronca/pen/ZyNQPX*/
