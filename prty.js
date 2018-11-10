940ad883fd6bf864ae74ac6a6a3391be2c460b58
Joe-Bruni <35151308+Joe-Bruni@users.noreply.github.com>
2018-11-10 15:53:58 -0500

    Da peppz

--- prty.js
+++ prty.js
@@ -4,5 +4,9 @@ function showDiv() {
    document.getElementById('hideDiv').style.display="none";
    document.getElementById('welcome').style.display="none";
 }
+
+function secret(){
+  document.getElementById('pepps').style.display="block";
+}
 /*https://codepen.io/anon/pen/wQMxBE*/
 /*https://codepen.io/rauldronca/pen/ZyNQPX*/
